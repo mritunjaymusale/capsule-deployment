b1094e2ec581bb2ddf9c16b941ef374c2b0d450d
mritunjaymusale <mritunjaymusale@gmail.com>
2018-12-17 15:55:24 +0530

    now connnected to the model

--- app.py
+++ app.py
@@ -1,65 +1,58 @@
 from flask import Flask, render_template, request, jsonify
-from scipy.misc import imsave, imread, imresize
+from scipy.misc import imsave, imread, imresize, imshow
 import numpy as np
 import re
 from CapsNet import capsules
 import torch
-
+from torchvision import transforms
+import base64
 device = torch.device("cpu")
-cuda=False
+cuda = False
 
 A, B, C, D = 64, 8, 16, 16
-
+# add cnn model as well
 model = capsules(A=A, B=B, C=C, D=D, E=10,
-                     iters=2,cuda=False).to(device)
-model.load_state_dict(torch.load('./saved_model/mnist.pth'))    
-model.eval()                 
+                 iters=2, cuda=False).to(device)
+model.load_state_dict(torch.load('./saved_model/mnist.pth'))
+model.eval()
 app = Flask(__name__)
 
 
 def convertImage(imgData1):
-    imgstr = re.search(r'base64,(.*)', imgData1).group(1)
+    imgstr = re.search(b'base64,(.*)', imgData1).group(1)
     # print(imgstr)
     with open('output.png', 'wb') as output:
-        output.write(imgstr.decode('base64'))
+        output.write(base64.b64decode(imgstr))
+
 
 @app.route('/', methods=['GET', 'POST'])
 def basic():
-    
+
     return render_template('index.html')
 
 
 @app.route('/predict/', methods=['GET', 'POST'])
 def predict():
-    #whenever the predict method is called, we're going
-    #to input the user drawn character as an image into the model
-    #perform inference, and return the classification
-    #get the raw data format of the image
-    imgData = request.get_data()
-    #encode it into a suitable format
     
-    convertImage(imgData)
-    print("debug")
-    #read the image into memory
-    x = imread('output.png', mode='L')
-    #compute a bit-wise inversion so black becomes white and vice versa
-    x = np.invert(x)
-    #make it the right size
-    x = imresize(x, (28, 28))
-    #imshow(x)
-    #convert to a 4D tensor to feed into our model
-    x = x.reshape(1, 28, 28, 1)
-    print("debug2")
-    #in our computation graph
+    imgData = request.get_data()
 
-    #perform the prediction
-    out = model.predict(x)
+    convertImage(imgData)
+
+    x = imread('output.png', mode='L')
+    x = np.invert(x)
+    x = imresize(x, (28, 28))
+    x = x.astype('float32')
+    x=x.reshape((1,1,28,28))
+    x = torch.from_numpy(x)
+    
+    # perform the prediction
+    out = model(x)
+    out =out.detach().numpy()
     print(out)
-    print(np.argmax(out, axis=1))
-    #print "debug3"
-    #convert the response to a string
-    response = np.array_str(np.argmax(out, axis=1))
+    # convert the response to a string
+    response = np.array_str(out)
     return response
 
+
 if __name__ == '__main__':
-    app.run(threaded=True,debug=True)
+    app.run(threaded=True, debug=True,host='0.0.0.0')
