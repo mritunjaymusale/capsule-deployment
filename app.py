32215a4ab27a4d178a36e863f5edc1a1f560a823
mritunjaymusale <mritunjaymusale@gmail.com>
2018-12-20 02:51:35 +0530

    capsules graph added

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
-from flask import Flask, render_template, request, jsonify
-from scipy.misc import imsave, imread, imresize, imshow
+from flask import Flask, render_template, request
+from scipy.misc import  imread, imresize
 import numpy as np
 import re
 from CapsNet import capsules
 import torch
-from torchvision import transforms
 import base64
 from CNN import Net
 from skimage import io
 import seaborn as sns
 import matplotlib.pyplot as plt
-
+import base64
 
 device = torch.device("cpu")
 cuda = False
@@ -23,7 +22,6 @@ capsules_model.load_state_dict(torch.load('./saved_model/mnist_capsules.pth'))
 capsules_model.eval()
 cnn_model = Net()
 cnn_model.load_state_dict(torch.load('./saved_model/mnist_cnn.pth'))
-
 app = Flask(__name__)
 
 
@@ -46,7 +44,7 @@ def predict():
     imgData = request.get_data()
 
     convertImage(imgData)
-    x = imread('output.png',mode='L')
+    x = imread('output.png', mode='L')
     x = np.invert(x)
     x = imresize(x, (28, 28))
     x = x.astype('float32')
@@ -59,14 +57,14 @@ def predict():
     caps = caps_output.flat
     cnn_output = cnn_model(x)
     cnn_output = cnn_output.detach().numpy()
-    values = [0,1,2,3,4,5,6,7,8,9]
+    values = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     plt.clf()
-    sns.barplot(x=values, y=list(caps))
-    plt.savefig('./static/caps_prob.png')
     
-    # convert the response to a string
-    response = "Capsules output :"+np.array_str(np.argmax(
-        caps_output, axis=1))+" CNN output :"+np.array_str(np.argmax(cnn_output, axis=1))
+    sns.barplot(x=values, y=list(caps))
+    plt.savefig('./static/capsules.png')
+    response =''
+    with open('./static/capsules.png','rb') as file:
+        response = base64.b64encode(file.read())
     return response
 
 
